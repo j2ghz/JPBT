67c32e239cf2b208191c399d8925b3249fee0146
J2ghz <1708197+j2ghz@users.noreply.github.com>
2026-03-17 16:16:53 +0100

    Initial Commit

---
+++ JPBT/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  JPBT
+//
+//  Created by Jozef Hollý on 17/03/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ JPBTTests/JPBTTests.swift
@@ -0,0 +1,16 @@
+//
+//  JPBTTests.swift
+//  JPBTTests
+//
+//  Created by Jozef Hollý on 17/03/2026.
+//
+
+import Testing
+
+struct JPBTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
